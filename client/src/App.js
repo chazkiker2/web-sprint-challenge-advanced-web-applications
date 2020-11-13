af1cd4d7cbe51e92741dcb195dc421ced077c08d
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-11-13 11:16:11 -0500

    setup project and resolve package.json

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,20 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
-import "./styles.scss";
+import "./styles.css";
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
+	return (
+		<Router>
+			<div className="App">
+				<Route exact path="/" component={Login} />
+				{/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
-      </div>
-    </Router>
-  );
+			</div>
+		</Router>
+	);
 }
 
 export default App;
